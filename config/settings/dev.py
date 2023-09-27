a0621208e2584bac762341d3e459ca3a8576fd33
Steffen P <steffen.pretzel@gmx.de>
2023-09-27 21:39:40 +0200

    Initial commit. Basic project structure created

---
+++ config/settings/dev.py
@@ -0,0 +1,34 @@
+from .base import *
+import environ
+
+
+env = environ.Env()
+environ.Env.read_env(str(BASE_DIR / ".env"))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env.str("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env.str("DB_NAME"),
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PWD"),
+        "HOST": env.str("DB_HOST"),
+        "PORT": env.int("DB_PORT"),
+    }
+}
